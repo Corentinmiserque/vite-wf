c80ba9c428147cadb9409e0137a249cea7f520ae
Corentinmiserque <corentinmiserque1@gmail.com>
2023-12-01 15:54:29 +0100

    modif

--- components/Display.js
+++ components/Display.js
@@ -1,18 +1,17 @@
 // display.js
 export default function displayBeers(beers) {
     const beerList = document.getElementById('beerList');
+    let beerListHTML = '';
     let totalABV = 0;
 
     beers.forEach(beer => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `<strong>${beer.name}</strong> ${beer.abv}%`;
-        beerList.appendChild(listItem);
-
+        beerListHTML += `<li><strong>${beer.name}</strong> ${beer.abv}%</li>`;
         totalABV += beer.abv;
     });
 
     const averageABV = totalABV / beers.length;
-    const averageABVElement = document.createElement('p');
-    averageABVElement.innerHTML = `Average ABV: ${averageABV.toFixed(2)}%`;
-    beerList.appendChild(averageABVElement);
+    beerListHTML += `<p>Average ABV: ${averageABV.toFixed(2)}%</p>`;
+
+    beerList.innerHTML = beerListHTML;
 }
+
